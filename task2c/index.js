a8f371267d1b4a14787659ff359706ca45f36519
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2022-12-15 19:38:02 +0000

    task 2c is passing

--- task2c/index.js
+++ task2c/index.js
@@ -1,32 +1,25 @@
 let correctAnswer = 70;
 
 function calc(numA, numB) {
-  // Define the double function
   function double(num) {
     return num * 2;
   }
-
-  // Define the square function
   function square(num) {
     return num * num;
   }
-
-  // Define the add and multiply functions
   function add(num1, num2) {
     return num1 + num2;
   }
   function multiply(num1, num2) {
     return num1 * num2;
   }
-
-  // Use the double and square functions in the calc function
   numA = double(numA); // 4
   numA = square(numA); // 16
   numA = multiply(numA, numB); // 64
   numA = add(1, numA); // 65
 
   // Use the add function to calculate the final answer
-  let answer = add(add(numA, numB), 1);
+  let answer = add(add(numA, numB), 1);//70
   if (correctAnswer === answer) {
     return (`Correct answer! You fixed all the bugs. Well done`);
   } else {
